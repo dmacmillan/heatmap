7b587c0fdc3e6c96cbb7cf55fc598efed7eb0c80
Daniel MacMillan <dmacmillan@dmacmillan01.phage.bcgsc.ca>
2016-12-06 08:12:14 -0800

    Removed functionality for multiple metrics, instead only use delta distances

--- heatmap.py
+++ heatmap.py
@@ -2,13 +2,13 @@ import os
 import sys
 import argparse
 import subprocess
+from scipy import stats
 
 parser = argparse.ArgumentParser('Create a matrix that can be loaded into R and generate a heatmap of a metric')
 
 parser.add_argument('gtex_compare_dir', help='Directory containing all comparisons made. Folders are named tissue1-vs-tissue2')
 parser.add_argument('ccle_compare_dir', help='Directory containing all comparisons made. Folders are named tissue1-vs-tissue2')
-parser.add_argument('-m', '--metrics', nargs='+', default=('skew'), choices=('mean', 'median', 'skew', 'mode', 'ratio', 'mannwhitneyu', 'delta'), help='A metric to generate the heatmap for')
-#parser.add_argument('-c', '--create', action='store_true', help='Whether or not to run R and generate a heatmap')
+#parser.add_argument('-m', '--metrics', nargs='+', default=('skew'), choices=('mean', 'median', 'skew', 'mode', 'ratio', 'mannwhitneyu', 'delta'), help='A metric to generate the heatmap for')
 parser.add_argument('-o', '--outdir', default=os.getcwd(), help='Path to output to')
 
 args = parser.parse_args()
@@ -18,6 +18,27 @@ if not os.path.isdir(args.outdir):
         os.makedirs(args.outdir)
     except OSError:
         pass
+
+def dicToMtx(dic, equal=1):
+    rownames = colnames = dic.keys()
+    length = len(rownames)
+    header = '\t' + ('\t').join(rownames) + '\n'
+    out = [header]
+    for i in xrange(length):
+        ki = rownames[i]
+        line = ['{}'.format(ki)]
+        for j in xrange(length):
+            kj = rownames[j]
+            if ki == kj:
+                line.append(equal)
+                continue
+            try:
+                line.append(dic[ki][kj])
+            except KeyError:
+                line.append(dic[kj][ki])
+        line = ('\t').join([str(x) for x in line])
+        out.append(line)
+    return ('\n').join(out)
 
 def parseDistances(_file):
     distances = []
@@ -36,70 +57,25 @@ def parseStats(path):
             res[line[0]] = float(line[1])
     return res
 
-results_gtex = {}
-
-for compare in os.listdir(args.gtex_compare_dir):
-    stats = os.path.join(args.gtex_compare_dir, compare, 'statistics')
-    #distances = os.path.join(args.gtex_compare_dir, compare, 'distances')
-    mwu = os.path.join(args.gtex_compare_dir, compare, 'mannwhitneyu')
-    stats = parseStats(stats)
-    #distances = parseDistances(distances)
-    stats['distances'] = distances
-    stats['mannwhitneyu'] = None
-    if 'mannwhitneyu' in args.metrics:
-        stats['mannwhitneyu'] = parseStats(mwu)['mannwhitneyu']
-    stats['ratio'] = abs(stats['total_pos'] / stats['total_neg'])
-    t1, t2 = compare.split('-vs-')
-    if t1 not in results_gtex:
-        results_gtex[t1] = {t2: stats}
-    elif t2 not in results_gtex[t1]:
-        results_gtex[t1][t2] = stats
-
-results_ccle = {}
+distance_mtx = {}
 
 for compare in os.listdir(args.ccle_compare_dir):
-    stats = os.path.join(args.ccle_compare_dir, compare, 'statistics')
-    #distances = os.path.join(args.gtex_compare_dir, compare, 'distances')
-    stats = parseStats(stats)
-    mwu = os.path.join(args.ccle_compare_dir, compare, 'mannwhitneyu')
-    stats['mannwhitneyu'] = None
-    #stats['distances'] = distances
-    if 'mannwhitneyu' in args.metrics:
-        stats['mannwhitneyu'] = parseStats(mwu)['mannwhitneyu']
-    stats['ratio'] = abs(float(stats['total_pos']) / stats['total_neg'])
+    distances_ccle = os.path.join(args.ccle_compare_dir, compare, 'distances')
+    distances_gtex = os.path.join(args.gtex_compare_dir, compare, 'distances')
+    distances_ccle = parseDistances(distances_ccle)
+    distances_gtex = parseDistances(distances_gtex)
+    pval = stats.mannwhitneyu(distances_ccle, distances_gtex)[1]
     t1, t2 = compare.split('-vs-')
-    if t1 not in results_ccle:
-        results_ccle[t1] = {t2: stats}
-    elif t2 not in results_ccle[t1]:
-        results_ccle[t1][t2] = stats
+    if t1 not in distance_mtx:
+        distance_mtx[t1] = {t2: pval}
+    elif t2 not in distance_mtx[t1]:
+        distance_mtx[t1][t2] = pval
 
-keys = sorted(results_ccle.keys())
-len_keys = len(keys)
-for metric in args.metrics:
-    out = os.path.join(args.outdir, metric + '.mtx')
-    with open(out, 'w') as o:
-        o.write('\t' + ('\t').join(keys) + '\n')
-        for i,t1 in enumerate(keys):
-            results = results_ccle
-            o.write(t1)
-            for j,t2 in enumerate(keys):
-                if t1 == t2:
-                    if metric == 'ratio':
-                        o.write('\t1')
-                    else:
-                        o.write('\t0')
-                    results = results_gtex
-                    continue
-                try:
-                    o.write('\t')
-                    o.write(str(results[t1][t2][metric]))
-                except KeyError:
-                    try:
-                        o.write('\t')
-                        o.write(str(results[t2][t1][metric]))
-                    except KeyError:
-                        o.write('\t0')
-            o.write('\n')
+result = dicToMtx(distance_mtx, equal=0)
+
+out = os.path.join(args.outdir, 'delta.mtx')
+with open(out, 'w') as o:
+    o.write(result)
 
 rscript = '/gsc/btl/linuxbrew/bin/Rscript'
 script = os.path.join(args.outdir, 'script.R')
@@ -107,13 +83,12 @@ with open(script, 'w') as o:
     o.write('library(pheatmap)\n')
     o.write('library(corrplot)\n')
     o.write('setwd(\'{}\')\n'.format(args.outdir))
-    for metric in args.metrics:
-        o.write('data_{} = read.table(\'{}.mtx\')\n'.format(metric, metric))
-        o.write('png(\'correlation_{}.png\', width=1000, height=1100, res=100)\n'.format(metric))
-        o.write('corrplot(cor(data_{}), method="number", type="upper", mar = c(4,4,4,4))\n'.format(metric))
-        o.write('dev.off()\n')
-        o.write('png(\'heatmap_{}.png\', width=1000, height=750, res=150)\n'.format(metric))
-        o.write('pheatmap(data_{}, cluster_rows=FALSE, cluster_cols=FALSE)\n'.format(metric))
-        o.write('dev.off()\n')
+    o.write('data_delta = read.table(\'delta.mtx\')\n')
+    o.write('png(\'correlation_delta.png\', width=1000, height=1100, res=100)\n')
+    o.write('corrplot(cor(data_delta), method="number", type="upper", mar = c(4,4,4,4))\n')
+    o.write('dev.off()\n')
+    o.write('png(\'heatmap_delta.png\', width=1000, height=750, res=150)\n')
+    o.write('pheatmap(data_delta, cluster_rows=FALSE, cluster_cols=FALSE)\n')
+    o.write('dev.off()\n')
 
 subprocess.check_call([rscript, script])
